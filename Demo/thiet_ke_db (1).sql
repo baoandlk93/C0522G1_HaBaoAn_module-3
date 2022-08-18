4f943b39a384baba9898935bb7f294ad6d6a1194
habaoan93 <baoandlk93@gmail.com>
2022-08-18 18:58:20 +0700

    18/08/2022

--- Demo/thiet_ke_db (1).sql
+++ Demo/thiet_ke_db (1).sql
@@ -156,93 +156,107 @@ where c.name ='c1121g1'
 order by s.point desc, s.name ;
 
 
+ -- bài 4: sử dụng các hàm thông dụng
 
+-- đếm số học viên toàn trung tâm
 
-
-
-
-
-
-
-
-
-
-
--- ss3 thao tác với csdl
--- 1. Lấy ra thông tin các học viên, và cho biết các học viên đang theo học lớp nào.
-select s.id,s.`name` as student_name,c.`name` as class_name
-from student s
-inner join class c
-on s.class_id = c.id;
-
-
--- 2. Lấy ra thông tin các học viên, và cho biết các học viên đang theo học lớp nào 
--- và cả các bạn đã đăng ký nhưng chưa có lớp học.
-select s.id,s.`name` as student_name,c.`name` as class_name
-from student s
-left join class c
-on s.class_id = c.id;
-
--- join 3 bảng lấy thêm thông tin tên loại lớp
-select s.id,s.`name` as student_name,c.`name` as class_name, ct.name as class_type_name
-from student s
-inner join class c
-on s.class_id = c.id
-join class_type ct
-on c.class_type_id = ct.id;
-
-
--- 4. Lấy thông tin của các học viên tên 'nguyen minh hai'.
-select * 
-from student s
-where s.name ='nguyen minh hai';
-
--- 4. Lấy thông tin của các học viên tên 'hai' và 'huynh’.
-select * 
-from student s
-where s.name like '% hai' or s.name like '% huynh';
-select * 
-from student s
-where s.name regexp '\\w+\\sh[a-z]+$';
--- 5. Lấy ra các học viên có điểm lớn hơn 5 .
-select *
+ select count(*) as so_luong
+ from student;
+ 
+ select * from student;
+ select * from class;
+-- 1.1	Hiện thị danh sách các lớp có học viên theo học và số lượng học viên của mỗi lớp
+select c.name as class_name, count(c.name) as so_luong
 from student s 
-where s.point >5;
+join class c on s.class_id = c.id
+group by c.name;
+
+-- 1.2.	 Tính điểm lớn nhất của mỗi các lớp
+select c.name as class_name, max(s.point) as diem_max
+from student s 
+join class c on s.class_id = c.id
+group by c.name;
+
+-- 1.3	 Tình điểm trung bình  của từng lớp 
+select c.name as class_name, avg(s.point) as diem_tb
+from student s 
+join class c on s.class_id = c.id
+group by c.name;
+
+
+
+
+ -- 2 Lấy ra toàn bộ tên và ngày sinh các instructor và student ở CodeGym.
+
+select  s.name, s.birthday from student s
+union
+select  i.name,i.birthday from instructor i;
+
+select count(*) from
+(select s.name as s_name
+from student s 
+left join class c on s.class_id = c.id
+union all
+select s.name as s_name
+from student s 
+right join class c on s.class_id = c.id) as tem;
+  -- 4 Lấy ra 3 học viên có điểm cao nhất của trung tâm.
+select *
+from student s
+order by s.point desc
+limit 3;
 
 select *
-from student s 
-having s.point >5;
--- lấy ra thông tin các học viên có điểm 4,6,8
-select *
-from student s 
-where s.point in(4,6,8);
+from student s
+order by s.point desc
+limit 2,1;
 
--- 6. Lấy ra học viên có họ là “nguyen”
+ -- 5. Lấy ra các học viên có điểm số là cao nhất của trung tâm.
+ -- dùng subquery
 select *
-from student s 
-where s.name like 'nguyen%';
--- 7. Thông kế số lượng học sinh theo từng loại điểm.
+from student s
+where s.point = (select max(point)from student);
 
-select s.point, count(s.point) as sl
-from student s 
-group by s.point;
--- 8 . Thông kế số lượng học sinh theo điểm và điểm phải lớn hơn 5
-select s.point, count(s.point) as sl
-from student s 
-where s.point >5
-group by s.point;
--- 9. Thông kế số lượng học sinh theo điểm lớn hơn 5 và chỉ hiện thị với số lượng>=2
-select s.point, count(s.point) as sl
-from student s 
-where s.point >5
-group by s.point
-having sl>=2;
--- 10. Lấy ra danh sách học viên của lớp c1121g1 và sắp xếp tên học viên theo point,
--- nếu point bằng nhau thì sắp xếp theo tên.
-select s.id, s.name,s.point,c.name as class_name
-from student s 
-join class c
-on s.class_id=c.id
-where c.name ="c1121g1"
-order by s.point,s.name desc
-;
+select * from instructor;
+select * from instructor_class;
+-- 6 tìm  những gv chưa từng tham gia giảng dạy;
+-- dùng join
+select i.name
+from instructor_class ic
+right join instructor i on ic.instructor_id=i.id
+where ic.instructor_id is null;
+
+-- dùng truy vấn con với i
+select * from instructor i where i.id not in(select distinct instructor_id
+from instructor_class );
+
+-- dùng truy vấn con với exist
+select *
+from instructor i
+where not exists (select *
+from instructor_class ic
+where ic.instructor_id=i.id);
+
+
+select * from class_type;
+select* from class;
+select* from jame;
+select * from student;
+select* from instructor;
+select * from instructor_class; 
+
+select* from class;
+select * from student;
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Demo/thiet_ke_db (2).sql
+++ Demo/thiet_ke_db (2).sql
@@ -156,93 +156,207 @@ where c.name ='c1121g1'
 order by s.point desc, s.name ;
 
 
+ -- bài 4: sử dụng các hàm thông dụng
 
+-- đếm số học viên toàn trung tâm
 
-
-
-
-
-
-
-
-
-
-
--- ss3 thao tác với csdl
--- 1. Lấy ra thông tin các học viên, và cho biết các học viên đang theo học lớp nào.
-select s.id,s.`name` as student_name,c.`name` as class_name
-from student s
-inner join class c
-on s.class_id = c.id;
-
-
--- 2. Lấy ra thông tin các học viên, và cho biết các học viên đang theo học lớp nào 
--- và cả các bạn đã đăng ký nhưng chưa có lớp học.
-select s.id,s.`name` as student_name,c.`name` as class_name
-from student s
-left join class c
-on s.class_id = c.id;
-
--- join 3 bảng lấy thêm thông tin tên loại lớp
-select s.id,s.`name` as student_name,c.`name` as class_name, ct.name as class_type_name
-from student s
-inner join class c
-on s.class_id = c.id
-join class_type ct
-on c.class_type_id = ct.id;
-
-
--- 4. Lấy thông tin của các học viên tên 'nguyen minh hai'.
-select * 
-from student s
-where s.name ='nguyen minh hai';
-
--- 4. Lấy thông tin của các học viên tên 'hai' và 'huynh’.
-select * 
-from student s
-where s.name like '% hai' or s.name like '% huynh';
-select * 
-from student s
-where s.name regexp '\\w+\\sh[a-z]+$';
--- 5. Lấy ra các học viên có điểm lớn hơn 5 .
-select *
+ select count(*) as so_luong
+ from student;
+ 
+ select * from student;
+ select * from class;
+-- 1.1	Hiện thị danh sách các lớp có học viên theo học và số lượng học viên của mỗi lớp
+select c.name as class_name, count(c.name) as so_luong
 from student s 
-where s.point >5;
+join class c on s.class_id = c.id
+group by c.name;
+
+-- 1.2.	 Tính điểm lớn nhất của mỗi các lớp
+select c.name as class_name, max(s.point) as diem_max
+from student s 
+join class c on s.class_id = c.id
+group by c.name;
+
+-- 1.3	 Tình điểm trung bình  của từng lớp 
+select c.name as class_name, avg(s.point) as diem_tb
+from student s 
+join class c on s.class_id = c.id
+group by c.name;
+
+
+
+
+ -- 2 Lấy ra toàn bộ tên và ngày sinh các instructor và student ở CodeGym.
+
+select  s.name, s.birthday from student s
+union
+select  i.name,i.birthday from instructor i;
+
+select count(*) from
+(select s.name as s_name
+from student s 
+left join class c on s.class_id = c.id
+union all
+select s.name as s_name
+from student s 
+right join class c on s.class_id = c.id) as tem;
+  -- 4 Lấy ra 3 học viên có điểm cao nhất của trung tâm.
+select *
+from student s
+order by s.point desc
+limit 3;
 
 select *
-from student s 
-having s.point >5;
--- lấy ra thông tin các học viên có điểm 4,6,8
-select *
-from student s 
-where s.point in(4,6,8);
+from student s
+order by s.point desc
+limit 2,1;
 
--- 6. Lấy ra học viên có họ là “nguyen”
+ -- 5. Lấy ra các học viên có điểm số là cao nhất của trung tâm.
+ -- dùng subquery
 select *
-from student s 
-where s.name like 'nguyen%';
--- 7. Thông kế số lượng học sinh theo từng loại điểm.
+from student s
+where s.point = (select max(point)from student);
 
-select s.point, count(s.point) as sl
-from student s 
-group by s.point;
--- 8 . Thông kế số lượng học sinh theo điểm và điểm phải lớn hơn 5
-select s.point, count(s.point) as sl
-from student s 
-where s.point >5
-group by s.point;
--- 9. Thông kế số lượng học sinh theo điểm lớn hơn 5 và chỉ hiện thị với số lượng>=2
-select s.point, count(s.point) as sl
-from student s 
-where s.point >5
-group by s.point
-having sl>=2;
--- 10. Lấy ra danh sách học viên của lớp c1121g1 và sắp xếp tên học viên theo point,
--- nếu point bằng nhau thì sắp xếp theo tên.
-select s.id, s.name,s.point,c.name as class_name
-from student s 
-join class c
-on s.class_id=c.id
-where c.name ="c1121g1"
-order by s.point,s.name desc
-;
+select * from instructor;
+select * from instructor_class;
+-- 6 tìm  những gv chưa từng tham gia giảng dạy;
+-- dùng join
+select i.name
+from instructor_class ic
+right join instructor i on ic.instructor_id=i.id
+where ic.instructor_id is null;
+
+-- dùng truy vấn con với i
+explain select * from instructor i where i.id not in(select distinct instructor_id
+from instructor_class );
+
+-- dùng truy vấn con với exist
+ explain select *
+from instructor i
+where not exists (select *
+from instructor_class ic
+where ic.instructor_id=i.id);
+
+
+select * from student s,class c
+where s.class_id = c.id ;
+
+insert into student(`name`,birthday, gender,`point`, class_id) 
+ values ('nguyen văn c','1981-12-12',1,8,1);
+delete from student;
+truncate student;
+
+-- ss5: index, view, sp, function, trigger
+-- 1. index
+select * from customers;
+explain select * from customers where city ='lyon';
+
+create index i_city on customers(city);
+drop index i_city on customers;
+alter table customers add index i_country(country);
+alter table customers drop index i_country;
+
+ select * from student;
+ select * from class;
+ select * from class_type;
+
+ -- 2.view (tạo view lấy thông tin của học viên)
+ create view w_student as
+select s.id, s.name, s.gender, s.email ,c.name as class_name,c.class_type_id from student s 
+join class c on s.class_id = c.id;
+create view w_class_type as
+select * from class_type;
+ -- sử dụng view
+select * from w_student s
+join w_class_type ct on s.class_type_id=ct.id ;
+
+ 
+-- 3.1  tạo một sp không tham số  để lấy danh sách student
+delimiter //
+create procedure sp_get_all_student()
+begin 
+select * from student;
+end //
+delimiter ;
+-- goi sp
+call sp_get_all_student();
+
+-- 3.2 sp có tham số in để lấy ra học viên có tham số id
+delimiter //
+create procedure sp_get_student_by_id(IN p_id int)
+begin 
+select * from student where id = p_id;
+end //
+delimiter ;
+ 
+ call sp_get_student_by_id(1);
+ call sp_get_student_by_id(12);
+
+-- 3.3 sp đếm số lượng học viên của trung tâm sử dụng OUT param
+delimiter //
+create procedure sp_count_student(out p_so_luong int)
+begin 
+select count(*) into p_so_luong from student;
+end //
+delimiter ;
+
+-- gọi sp
+call sp_count_student(@sl);
+select @sl;
+
+-- 4 viêt 1 function xếp loại học lưc theo point
+
+-- sử dụng function
+
+
+-- 5 tạo trigger tự động tạo tài khoản jame trước khi thêm mới một học viên
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
